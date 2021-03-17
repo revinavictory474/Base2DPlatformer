6cae9def1d3d8ccec053e443f288c020f74c8f52
Victory <revina.victory@gmail.com>
2021-03-17 13:36:33 +0700

    V 0.1
    jump, platform, gravity

--- Assets/Code/PlatformerPlayer.cs
+++ Assets/Code/PlatformerPlayer.cs
@@ -6,11 +6,14 @@ public class PlatformerPlayer : MonoBehaviour
     public float speed = 250.0f;
     private Rigidbody2D _body;
     private Animator _anim;
+    public float jumpForce = 12.0f;
+    private BoxCollider2D _box;
 
     private void Start()
     {
         _body = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
+        _box = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -18,6 +21,23 @@ public class PlatformerPlayer : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         Vector2 movement = new Vector2(deltaX, _body.velocity.y);
         _body.velocity = movement;
+
+        Vector3 max = _box.bounds.max;
+        Vector3 min = _box.bounds.min;
+
+        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+
+        bool grounded = false;
+        if (hit != null) grounded = true;
+
+        _body.gravityScale = grounded && deltaX == 0 ? 0 : 1;
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
 
         _anim.SetFloat("speed", Mathf.Abs(deltaX));
         if (!Mathf.Approximately(deltaX, 0))
